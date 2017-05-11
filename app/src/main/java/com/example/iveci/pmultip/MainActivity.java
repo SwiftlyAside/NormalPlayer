42f97d09a91977bbc977dac1cab1ced2b96035fd
Side <ivecis@live.co.kr>
2017-05-11 13:00:38 +0900

    Initial commit

---
+++ app/src/main/java/com/example/iveci/pmultip/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.iveci.pmultip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/*
+* MultiP
+* Description:
+* 뭔가를 재생하는 프로그램.
+* Functions:
+* 미확정
+*
+* Used other libraries:
+*
+* ETC:
+* 이 프로그램에서 상단바는 쓰지 않음.
+* 
+* */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
