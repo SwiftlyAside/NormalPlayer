cc7d39d79eaf0272b51133a0046d5470c9e496c4
Side <ivecis@live.co.kr>
2017-05-27 23:21:41 +0900

    v0.0527
    재생테스트용 음원 포함.

--- app/src/main/java/com/example/iveci/pmultip/MainActivity.java
+++ app/src/main/java/com/example/iveci/pmultip/MainActivity.java
@@ -22,16 +22,28 @@ import android.widget.TextView;
 * 
 * */
 public class MainActivity extends AppCompatActivity {
-    MediaPlayer mp;
+    MediaPlayer player;
     ImageView albart;
     SeekBar timeseek;
     TextView sinfo, ainfo;
     boolean play = false;
 
+    class mps extends Thread {
+        @Override
+        public void run() {
+            while(play){ //재생중인 경우, 탐색바를 움직입니다.
+                timeseek.setProgress(player.getCurrentPosition());
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        timeseek = (SeekBar) findViewById(R.id.timeseek);
+        player = MediaPlayer.create(getApplicationContext(),
+                R.raw.diamond);
     }
 
     public void onClick(View v){
@@ -40,6 +52,30 @@ public class MainActivity extends AppCompatActivity {
                 break;
             }
             case R.id.bstst :{
+                /*
+                재생/일시정지.
+
+                고려해야 할 사항.
+
+                플레이 안하고 있을때
+                일시정지일때
+                플레이중일때
+                 */
+                if(!player.isPlaying() && player.getCurrentPosition() != 0){ //일시정지일때
+                    play = true;
+                    player.start();
+                }
+                else if(!player.isPlaying()) { //플레이 안하고 있을때
+                    player.setLooping(false);
+                    play = true;
+                    player.start();
+                    timeseek.setMax(player.getDuration());
+                    new mps().start();
+                }
+                else { //플레이중일때
+                    play = false;
+                    player.pause();
+                }
                 break;
             }
             case R.id.bnext :{
