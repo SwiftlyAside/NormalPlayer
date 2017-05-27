66c62b9e0b7d14116c06a1a6e09e6f88008b1fd3
Side <ivecis@live.co.kr>
2017-05-27 22:40:30 +0900

    v0.0527

--- app/src/main/java/com/example/iveci/pmultip/MainActivity.java
+++ app/src/main/java/com/example/iveci/pmultip/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.iveci.pmultip;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
 /*
 * MultiP
 * Description:
@@ -16,10 +22,29 @@ import android.os.Bundle;
 * 
 * */
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mp;
+    ImageView albart;
+    SeekBar timeseek;
+    TextView sinfo, ainfo;
+    boolean play = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.bprev :{
+                break;
+            }
+            case R.id.bstst :{
+                break;
+            }
+            case R.id.bnext :{
+                break;
+            }
+        }
     }
 }
