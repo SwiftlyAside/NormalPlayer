857a0d8cc320aced6c558416d271de8180580cc4
Side <ivecis@live.co.kr>
2017-06-18 10:35:53 +0900

    v0.9.0618
    Fix permission request

--- app/src/main/java/com/ivsa/normalplayer/Tab.java
+++ app/src/main/java/com/ivsa/normalplayer/Tab.java
@@ -1,8 +1,10 @@
 package com.ivsa.normalplayer;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -22,7 +24,7 @@ import android.widget.Toast;
 
 
 public class Tab extends AppCompatActivity{
-
+    public final int PERMISSION_WRITE = 100;
     TabLayout tabLayout;
     ViewPager viewPager;
     ViewPagerAdapter adapter;
@@ -35,28 +37,50 @@ public class Tab extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        contextOfApplication = getApplicationContext();
-        int permissioninfo = ContextCompat.checkSelfPermission(this,
-                android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (permissioninfo == PackageManager.PERMISSION_DENIED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE)){
-                Toast.makeText(getApplicationContext(),
-                        "SDCard 쓰기 권한이 필요합니다. \n" + "설정에서 수동으로 활성화해주세요.",Toast.LENGTH_SHORT).show();
-            }
-            else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
-            }
-        }
         setContentView(R.layout.activity_tab);
+        contextOfApplication = getApplicationContext();
         tabLayout = (TabLayout) findViewById(R.id.tab);
         viewPager = (ViewPager) findViewById(R.id.pager);
         adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment("음악", new FragmentExplorer());
-        adapter.addFragment("재생목록", new FragmentPlaylist());
-        viewPager.setAdapter(adapter);
-        tabLayout.setupWithViewPager(viewPager);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                Toast.makeText(getApplicationContext(),
+                        "미디어를 불러오려면 SDCard 쓰기 권한이 필요합니다. \n",Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_WRITE);
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_WRITE);
+
+            }
+        }
+        else {
+            adapter.addFragment("음악", new FragmentExplorer());
+            adapter.addFragment("재생목록", new FragmentPlaylist());
+            viewPager.setAdapter(adapter);
+            tabLayout.setupWithViewPager(viewPager);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_WRITE : {
+                if (grantResults.length > 0  && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    adapter.addFragment("음악", new FragmentExplorer());
+                    adapter.addFragment("재생목록", new FragmentPlaylist());
+                    viewPager.setAdapter(adapter);
+                    tabLayout.setupWithViewPager(viewPager);
+                    break;
+                }
+            }
+        }
     }
 
     public void onClick(View v) {
