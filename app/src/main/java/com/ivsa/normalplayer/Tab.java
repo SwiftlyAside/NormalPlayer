0f56f67a8ff7dc32f685f3ce4d1caa2485564a7b
SwiftlyAside <ivecis@live.co.kr>
2021-07-30 17:51:04 +0900

    v0.10.5 Some fixes

--- app/src/main/java/com/ivsa/normalplayer/Tab.java
+++ app/src/main/java/com/ivsa/normalplayer/Tab.java
@@ -70,6 +70,7 @@ public class Tab extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == PERMISSION_WRITE)
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 setViewPager();
